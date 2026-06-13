8d08a6a9caf60b3637fa1bee9c4ed0df3f1c9827
Tacs <act.rtacangjr@gmail.com>
2026-06-13 11:02:34 +0800

    refactor: refactor product functionalities

--- server/src/services/product.service.ts
+++ server/src/services/product.service.ts
@@ -1,7 +1,27 @@
 import {connection} from "../data/db.js"
-import type {Item} from "../types/product.type.js"
+import {
+    deductStockByProductIdRepository,
+    getAllProductsRepository,
+    getProductByIdRepository,
+} from "../repositories/product.repository.js"
+import type {Product} from "../types/product.type.js"
 
-export const getAllProductsService = async (): Promise<Item[]> => {
-    const [rows] = await connection.query<Item[]>("SELECT * FROM Item")
-    return rows
+export const getAllProductsService = async (): Promise<Product[]> => {
+    const conn = await connection.getConnection()
+    try {
+        return await getAllProductsRepository(conn)
+    } finally {
+        conn.release()
+    }
+}
+
+export const getProductByIdService = async (
+    productId: number,
+): Promise<Product | null> => {
+    const conn = await connection.getConnection()
+    try {
+        return await getProductByIdRepository(conn, productId)
+    } finally {
+        conn.release()
+    }
 }
