3724e08c951ab93f3a115b570f8d27a7731be6f9
Tacs <act.rtacangjr@gmail.com>
2026-06-13 11:08:23 +0800

    feat: specify origin

--- server/src/server.ts
+++ server/src/server.ts
@@ -6,7 +6,12 @@ import {exceptionMiddleware} from "./middlewares/exception.middleware.js"
 
 const app = express()
 
-app.use(cors())
+app.use(
+    cors({
+        origin: "http://localhost:5173",
+        credentials: true,
+    }),
+)
 app.use(express.json())
 
 app.use("/api/products", ProductRoute)
